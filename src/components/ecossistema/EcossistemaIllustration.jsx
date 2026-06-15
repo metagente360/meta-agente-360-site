405baeddc6aa590d72ef389250d2371b41a63bdd
LMZ Inovação <lmzinovacao@gmail.com>
2026-06-15 21:25:03 +0000

    File changes

--- src/components/ecossistema/EcossistemaIllustration.jsx
+++ src/components/ecossistema/EcossistemaIllustration.jsx
@@ -9,9 +9,9 @@ export default function EcossistemaIllustration() {
       transition={{ duration: 0.7, ease: [0.22, 1, 0.36, 1] }}
     >
       <img
-        src="https://media.base44.com/images/public/6a3057094f64de9f1f72d897/948068ac0_ChatGPTImage15dejunde202618_21_10.png"
+        src="https://media.base44.com/images/public/6a3057094f64de9f1f72d897/e35cbff0c_ecossistema.png"
         alt="Ecossistema Meta Agente 360"
-        className="w-full h-full object-contain scale-150"
+        className="w-full h-auto object-contain"
         style={{ mixBlendMode: 'multiply' }}
       />
     </motion.div>
